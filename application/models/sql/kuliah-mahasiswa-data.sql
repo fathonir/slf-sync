3c5a341e469a90b87ac720845d5242b3a0354406
Fathoni <m.fathoni@mail.com>
2020-02-23 22:06:12 +0700

    ubah query aktivitas kuliah mahasiswa agar membaca tagihan keuangan

--- application/models/sql/kuliah-mahasiswa-data.sql
+++ application/models/sql/kuliah-mahasiswa-data.sql
@@ -16,16 +16,22 @@ WHERE pt.npsn = '@npsn' AND ms.id_semester = '@id_semester' AND ms.fd_id_smt IS 
 UNION ALL
 /* Jumlah Bakal Update */
 SELECT count(*) AS jumlah FROM mahasiswa_status ms
+LEFT JOIN status_pengguna sp ON sp.id_status_pengguna = ms.id_status_pengguna
 JOIN mahasiswa m ON m.id_mhs = ms.id_mhs
 JOIN program_studi ps ON ps.id_program_studi = m.id_program_studi
 JOIN fakultas f ON f.id_fakultas = ps.id_fakultas
 JOIN perguruan_tinggi pt ON pt.id_perguruan_tinggi = f.id_perguruan_tinggi
-WHERE pt.npsn = '@npsn' AND ms.id_semester = '@id_semester' AND ms.fd_id_smt IS NOT NULL AND ms.fd_id_reg_pd IS NOT NULL AND ms.fd_sync_on < ms.updated_on
+WHERE pt.npsn = '@npsn' AND ms.id_semester = '@id_semester' AND 
+    sp.fd_id_stat_mhs IS NOT NULL AND /* Pembatasan Status Aktivitas Kuliah */
+    ms.fd_id_smt IS NOT NULL AND ms.fd_id_reg_pd IS NOT NULL AND ms.fd_sync_on < ms.updated_on
 UNION ALL
 /* Jumlah Bakal Insert */
 SELECT count(*) AS jumlah FROM mahasiswa_status ms
+LEFT JOIN status_pengguna sp ON sp.id_status_pengguna = ms.id_status_pengguna
 JOIN mahasiswa m ON m.id_mhs = ms.id_mhs
 JOIN program_studi ps ON ps.id_program_studi = m.id_program_studi
 JOIN fakultas f ON f.id_fakultas = ps.id_fakultas
 JOIN perguruan_tinggi pt ON pt.id_perguruan_tinggi = f.id_perguruan_tinggi
-WHERE pt.npsn = '@npsn' AND ms.id_semester = '@id_semester' AND ms.fd_id_smt IS NULL AND ms.fd_id_reg_pd IS NULL
+WHERE pt.npsn = '@npsn' AND ms.id_semester = '@id_semester' AND
+    sp.fd_id_stat_mhs IS NOT NULL AND /* Pembatasan Status Aktivitas Kuliah */
+    ms.fd_id_smt IS NULL AND ms.fd_id_reg_pd IS NULL
--- application/models/sql/kuliah-mahasiswa-insert.sql
+++ application/models/sql/kuliah-mahasiswa-insert.sql
@@ -1,13 +1,17 @@
 SELECT 
     ms.id_mhs_status, m.nim_mhs,
-    '@id_smt' as id_smt, m.fd_id_reg_pd as id_reg_pd, sp.fd_id_stat_mhs as id_stat_mhs, ips, sks_semester as sks_smt, ipk, sks_total
+    '@id_smt' as id_smt, m.fd_id_reg_pd as id_reg_pd, sp.fd_id_stat_mhs as id_stat_mhs, 
+    ips, sks_semester as sks_smt, ipk, sks_total, 
+    tm.total_besar_biaya as biaya_smt
 FROM mahasiswa_status ms
 JOIN mahasiswa m ON m.id_mhs = ms.id_mhs
 LEFT JOIN status_pengguna sp ON sp.id_status_pengguna = ms.id_status_pengguna
 JOIN semester s ON s.id_semester = ms.id_semester
 JOIN perguruan_tinggi pt ON pt.id_perguruan_tinggi = s.id_perguruan_tinggi
+LEFT JOIN tagihan_mhs tm ON tm.id_mhs = m.id_mhs AND tm.id_semester = s.id_semester
 WHERE 
     pt.npsn = '@npsn' AND
     s.thn_akademik_semester||decode(nm_semester,'Ganjil',1,'Genap',2,'Pendek',3) = '@id_smt' AND
+    sp.fd_id_stat_mhs IS NOT NULL AND /* Pembatasan Status Aktivitas Kuliah */
     ms.fd_id_smt IS NULL AND ms.fd_id_reg_pd IS NULL
 ORDER BY m.nim_mhs
