2908b8316b918b5fc9b6f2242e0bf0ecd82df812
Fathoni <m.fathoni@mail.com>
2020-03-22 21:01:46 +0700

    membuat tagihan 0 untuk mahasiswa yang berstatus cuti

--- application/models/sql/kuliah-mahasiswa-insert.sql
+++ application/models/sql/kuliah-mahasiswa-insert.sql
@@ -2,7 +2,7 @@ SELECT
     ms.id_mhs_status, m.nim_mhs,
     '@id_smt' as id_smt, m.fd_id_reg_pd as id_reg_pd, sp.fd_id_stat_mhs as id_stat_mhs, 
     ips, sks_semester as sks_smt, ipk, sks_total, 
-    tm.total_besar_biaya as biaya_smt
+    CASE sp.fd_id_stat_mhs WHEN 'C' THEN 0 ELSE tm.total_besar_biaya END as biaya_smt
 FROM mahasiswa_status ms
 JOIN mahasiswa m ON m.id_mhs = ms.id_mhs
 LEFT JOIN status_pengguna sp ON sp.id_status_pengguna = ms.id_status_pengguna
